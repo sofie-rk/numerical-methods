123166d0cf2bdfa837fd26198ff91541743f2722
sofie-rk <sofierk@stud.ntnu.no>
2020-08-09 18:57:48 +0200

    RK4

--- ordinary-differential-equations-numerical-solution/README.md
+++ ordinary-differential-equations-numerical-solution/README.md
@@ -2,6 +2,9 @@
 
 Solve initial value problem (IVP) y'(t)=f(t, y(t)) with initial value y(t=0)=y0
 
+## Runge-Kutta methods
+All methods presented here are Runge-Kutta methods.
+
 ## Euler's method
 One step method.
 Explicit method.
@@ -15,4 +18,14 @@ More accurate than Euler's method
 k1 = f(t_i, y_i)
 k2 = f(t_(i+1), y_i + h*f(t_i, y_i)) = f(t_(i+1), y_i + h*k2)
 y_(i+1) = y_i + h/2 * (f(t_i, y_i) + f(t_(i+1), y_i + h*f(t_i, y_i))) = y_i + h/2 * (k1 + k2)
+
+## RK 4
+k1 = f(t_i, y_i)
+k2 = f(t_i + h/2, y_i + h/2*k1)
+k3 = f(t_i + h/2, y_i + h/2*k2)
+k4 = f(t_i + h, y_i + h*k3)
+
+y_(i+1) = y_i + h/6 * (k1 + 2*k2 + 2*k3 + k4)
+
+
 
--- ordinary-differential-equations-numerical-solution/runge-kutta-4.py
+++ ordinary-differential-equations-numerical-solution/runge-kutta-4.py
@@ -1,1 +1,52 @@
-# In development
+### RK4 ###
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def RK4(y0, t0, T, f, N):
+    ts = [t0]
+    ys = [y0]
+    dt = (T-t0)/N
+
+    while ts[-1]<T:
+        t, y = ts[-1], ys[-1]
+        k1 = f(t,y)
+        k2 = f(t+0.5*dt, y+0.5*dt*k1)
+        k3 = f(t+0.5*dt, y+0.5*dt*k2)
+        k4 = f(t+dt, y+dt*k3)
+    
+        
+        ys.append(y + dt/6 * (k1 + 2*k2 + 2*k3 + k4))
+        ts.append(t + dt)
+
+    return np.array(ts), np.array(ys)
+
+# Example y'(t) = -y(t)
+t0 = 0
+T = 1
+y0 = 1
+N = 4
+f = lambda t,y: y
+ts, ys_rk4 = RK4(y0, t0, T, f, N)
+
+# Will compare to the exact solution, y(t) = y0 * exp(-t)
+y_exact = lambda t: y0*np.exp(t-t0)
+ys_exact = y_exact(ts)
+
+plt.plot(ts, ys_rk4, "ro", label="Numerical solution")
+plt.plot(ts, ys_exact, label="Exact solution")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.legend()
+plt.show()
+
+# Example where you can see the effect of N
+N_list = [4, 8]
+color_list = ["green", "red", "yellow", "black"]
+plt.plot(ts, ys_exact, label="Exact solution", color="blue")
+for i in range(len(N_list)):
+    ts, ys_rk4 = RK4(y0, t0, T, f, N_list[i])
+    description = "N =" + str(N_list[i])
+    plt.plot(ts, ys_rk4, 'ro', label=description, color=color_list[i])
+plt.legend() 
+plt.show()
